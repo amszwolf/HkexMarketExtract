8981990f3eb1d39834ed69bf5186ddf534f7ef17
gavincyi <gavincyi@gmail.com>
2016-02-23 07:43:22 +0800

    Updated release config

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -56,8 +56,8 @@ TESTFILES= \
 CFLAGS=
 
 # CC Compiler Flags
-CCFLAGS=
-CXXFLAGS=
+CCFLAGS=-std=c++11
+CXXFLAGS=-std=c++11
 
 # Fortran Compiler Flags
 FFLAGS=
@@ -66,7 +66,7 @@ FFLAGS=
 ASFLAGS=
 
 # Link Libraries and Options
-LDLIBSOPTIONS=
+LDLIBSOPTIONS=-L/usr/local/lib -L/usr/lib64/mysql -lboost_date_time -lboost_system -lboost_thread -lboost_regex -lmysqlpp -lmysqlclient -llog4cplus
 
 # Build Targets
 .build-conf: ${BUILD_SUBPROJECTS}
@@ -76,50 +76,50 @@ ${TESTDIR}/TestFiles/f2: ${OBJECTFILES}
 	${MKDIR} -p ${TESTDIR}/TestFiles
 	${LINK.cc} -o ${TESTDIR}/TestFiles/f2 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/DataExtract/FutureInstrument.o: DataExtract/FutureInstrument.cpp 
+${OBJECTDIR}/DataExtract/FutureInstrument.o: nbproject/Makefile-${CND_CONF}.mk DataExtract/FutureInstrument.cpp 
 	${MKDIR} -p ${OBJECTDIR}/DataExtract
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/FutureInstrument.o DataExtract/FutureInstrument.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/FutureInstrument.o DataExtract/FutureInstrument.cpp
 
-${OBJECTDIR}/DataExtract/HkexExtractor.o: DataExtract/HkexExtractor.cpp 
+${OBJECTDIR}/DataExtract/HkexExtractor.o: nbproject/Makefile-${CND_CONF}.mk DataExtract/HkexExtractor.cpp 
 	${MKDIR} -p ${OBJECTDIR}/DataExtract
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/HkexExtractor.o DataExtract/HkexExtractor.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/HkexExtractor.o DataExtract/HkexExtractor.cpp
 
-${OBJECTDIR}/DataExtract/Instrument.o: DataExtract/Instrument.cpp 
+${OBJECTDIR}/DataExtract/Instrument.o: nbproject/Makefile-${CND_CONF}.mk DataExtract/Instrument.cpp 
 	${MKDIR} -p ${OBJECTDIR}/DataExtract
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/Instrument.o DataExtract/Instrument.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/Instrument.o DataExtract/Instrument.cpp
 
-${OBJECTDIR}/Util/UtilConfigAccess.o: Util/UtilConfigAccess.cpp 
+${OBJECTDIR}/Util/UtilConfigAccess.o: nbproject/Makefile-${CND_CONF}.mk Util/UtilConfigAccess.cpp 
 	${MKDIR} -p ${OBJECTDIR}/Util
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilConfigAccess.o Util/UtilConfigAccess.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilConfigAccess.o Util/UtilConfigAccess.cpp
 
-${OBJECTDIR}/Util/UtilDateTime.o: Util/UtilDateTime.cpp 
+${OBJECTDIR}/Util/UtilDateTime.o: nbproject/Makefile-${CND_CONF}.mk Util/UtilDateTime.cpp 
 	${MKDIR} -p ${OBJECTDIR}/Util
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilDateTime.o Util/UtilDateTime.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilDateTime.o Util/UtilDateTime.cpp
 
-${OBJECTDIR}/Util/UtilHttpRequestHandler.o: Util/UtilHttpRequestHandler.cpp 
+${OBJECTDIR}/Util/UtilHttpRequestHandler.o: nbproject/Makefile-${CND_CONF}.mk Util/UtilHttpRequestHandler.cpp 
 	${MKDIR} -p ${OBJECTDIR}/Util
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilHttpRequestHandler.o Util/UtilHttpRequestHandler.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilHttpRequestHandler.o Util/UtilHttpRequestHandler.cpp
 
-${OBJECTDIR}/Util/UtilLogger.o: Util/UtilLogger.cpp 
+${OBJECTDIR}/Util/UtilLogger.o: nbproject/Makefile-${CND_CONF}.mk Util/UtilLogger.cpp 
 	${MKDIR} -p ${OBJECTDIR}/Util
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilLogger.o Util/UtilLogger.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilLogger.o Util/UtilLogger.cpp
 
-${OBJECTDIR}/Util/UtilMysqlClient.o: Util/UtilMysqlClient.cpp 
+${OBJECTDIR}/Util/UtilMysqlClient.o: nbproject/Makefile-${CND_CONF}.mk Util/UtilMysqlClient.cpp 
 	${MKDIR} -p ${OBJECTDIR}/Util
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilMysqlClient.o Util/UtilMysqlClient.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilMysqlClient.o Util/UtilMysqlClient.cpp
 
-${OBJECTDIR}/main.o: main.cpp 
+${OBJECTDIR}/main.o: nbproject/Makefile-${CND_CONF}.mk main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -O2 -IUtil -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
 
 # Subprojects
 .build-subprojects:
@@ -128,49 +128,49 @@ ${OBJECTDIR}/main.o: main.cpp
 .build-tests-conf: .build-conf ${TESTFILES}
 ${TESTDIR}/TestFiles/f1: ${TESTDIR}/DataExtract/DataExtractTest/FutureInstrumentTest.o ${TESTDIR}/Util/RunAllTests.o ${TESTDIR}/Util/UtilTest/UtilConfigAccessTest.o ${TESTDIR}/Util/UtilTest/UtilDateTimeTest.o ${TESTDIR}/Util/UtilTest/UtilHttpRequestHandlerTest.o ${TESTDIR}/Util/UtilTest/UtilLoggerTest.o ${TESTDIR}/Util/UtilTest/UtilMysqlClientTest.o ${OBJECTFILES:%.o=%_nomain.o}
 	${MKDIR} -p ${TESTDIR}/TestFiles
-	${LINK.cc}   -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS} 
+	${LINK.cc}   -o ${TESTDIR}/TestFiles/f1 $^ ${LDLIBSOPTIONS} -lboost_unit_test_framework 
 
 
 ${TESTDIR}/DataExtract/DataExtractTest/FutureInstrumentTest.o: DataExtract/DataExtractTest/FutureInstrumentTest.cpp 
 	${MKDIR} -p ${TESTDIR}/DataExtract/DataExtractTest
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/DataExtract/DataExtractTest/FutureInstrumentTest.o DataExtract/DataExtractTest/FutureInstrumentTest.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/DataExtract/DataExtractTest/FutureInstrumentTest.o DataExtract/DataExtractTest/FutureInstrumentTest.cpp
 
 
 ${TESTDIR}/Util/RunAllTests.o: Util/RunAllTests.cpp 
 	${MKDIR} -p ${TESTDIR}/Util
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/RunAllTests.o Util/RunAllTests.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/RunAllTests.o Util/RunAllTests.cpp
 
 
 ${TESTDIR}/Util/UtilTest/UtilConfigAccessTest.o: Util/UtilTest/UtilConfigAccessTest.cpp 
 	${MKDIR} -p ${TESTDIR}/Util/UtilTest
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilConfigAccessTest.o Util/UtilTest/UtilConfigAccessTest.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilConfigAccessTest.o Util/UtilTest/UtilConfigAccessTest.cpp
 
 
 ${TESTDIR}/Util/UtilTest/UtilDateTimeTest.o: Util/UtilTest/UtilDateTimeTest.cpp 
 	${MKDIR} -p ${TESTDIR}/Util/UtilTest
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilDateTimeTest.o Util/UtilTest/UtilDateTimeTest.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilDateTimeTest.o Util/UtilTest/UtilDateTimeTest.cpp
 
 
 ${TESTDIR}/Util/UtilTest/UtilHttpRequestHandlerTest.o: Util/UtilTest/UtilHttpRequestHandlerTest.cpp 
 	${MKDIR} -p ${TESTDIR}/Util/UtilTest
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilHttpRequestHandlerTest.o Util/UtilTest/UtilHttpRequestHandlerTest.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilHttpRequestHandlerTest.o Util/UtilTest/UtilHttpRequestHandlerTest.cpp
 
 
 ${TESTDIR}/Util/UtilTest/UtilLoggerTest.o: Util/UtilTest/UtilLoggerTest.cpp 
 	${MKDIR} -p ${TESTDIR}/Util/UtilTest
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilLoggerTest.o Util/UtilTest/UtilLoggerTest.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilLoggerTest.o Util/UtilTest/UtilLoggerTest.cpp
 
 
 ${TESTDIR}/Util/UtilTest/UtilMysqlClientTest.o: Util/UtilTest/UtilMysqlClientTest.cpp 
 	${MKDIR} -p ${TESTDIR}/Util/UtilTest
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilMysqlClientTest.o Util/UtilTest/UtilMysqlClientTest.cpp
+	$(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -IDataExtract -I. -MMD -MP -MF "$@.d" -o ${TESTDIR}/Util/UtilTest/UtilMysqlClientTest.o Util/UtilTest/UtilMysqlClientTest.cpp
 
 
 ${OBJECTDIR}/DataExtract/FutureInstrument_nomain.o: ${OBJECTDIR}/DataExtract/FutureInstrument.o DataExtract/FutureInstrument.cpp 
@@ -181,7 +181,7 @@ ${OBJECTDIR}/DataExtract/FutureInstrument_nomain.o: ${OBJECTDIR}/DataExtract/Fut
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/FutureInstrument_nomain.o DataExtract/FutureInstrument.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/FutureInstrument_nomain.o DataExtract/FutureInstrument.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/DataExtract/FutureInstrument.o ${OBJECTDIR}/DataExtract/FutureInstrument_nomain.o;\
 	fi
@@ -194,7 +194,7 @@ ${OBJECTDIR}/DataExtract/HkexExtractor_nomain.o: ${OBJECTDIR}/DataExtract/HkexEx
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/HkexExtractor_nomain.o DataExtract/HkexExtractor.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/HkexExtractor_nomain.o DataExtract/HkexExtractor.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/DataExtract/HkexExtractor.o ${OBJECTDIR}/DataExtract/HkexExtractor_nomain.o;\
 	fi
@@ -207,7 +207,7 @@ ${OBJECTDIR}/DataExtract/Instrument_nomain.o: ${OBJECTDIR}/DataExtract/Instrumen
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/Instrument_nomain.o DataExtract/Instrument.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -IUtil -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/DataExtract/Instrument_nomain.o DataExtract/Instrument.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/DataExtract/Instrument.o ${OBJECTDIR}/DataExtract/Instrument_nomain.o;\
 	fi
@@ -220,7 +220,7 @@ ${OBJECTDIR}/Util/UtilConfigAccess_nomain.o: ${OBJECTDIR}/Util/UtilConfigAccess.
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilConfigAccess_nomain.o Util/UtilConfigAccess.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilConfigAccess_nomain.o Util/UtilConfigAccess.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/Util/UtilConfigAccess.o ${OBJECTDIR}/Util/UtilConfigAccess_nomain.o;\
 	fi
@@ -233,7 +233,7 @@ ${OBJECTDIR}/Util/UtilDateTime_nomain.o: ${OBJECTDIR}/Util/UtilDateTime.o Util/U
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilDateTime_nomain.o Util/UtilDateTime.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilDateTime_nomain.o Util/UtilDateTime.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/Util/UtilDateTime.o ${OBJECTDIR}/Util/UtilDateTime_nomain.o;\
 	fi
@@ -246,7 +246,7 @@ ${OBJECTDIR}/Util/UtilHttpRequestHandler_nomain.o: ${OBJECTDIR}/Util/UtilHttpReq
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilHttpRequestHandler_nomain.o Util/UtilHttpRequestHandler.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilHttpRequestHandler_nomain.o Util/UtilHttpRequestHandler.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/Util/UtilHttpRequestHandler.o ${OBJECTDIR}/Util/UtilHttpRequestHandler_nomain.o;\
 	fi
@@ -259,7 +259,7 @@ ${OBJECTDIR}/Util/UtilLogger_nomain.o: ${OBJECTDIR}/Util/UtilLogger.o Util/UtilL
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilLogger_nomain.o Util/UtilLogger.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilLogger_nomain.o Util/UtilLogger.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/Util/UtilLogger.o ${OBJECTDIR}/Util/UtilLogger_nomain.o;\
 	fi
@@ -272,7 +272,7 @@ ${OBJECTDIR}/Util/UtilMysqlClient_nomain.o: ${OBJECTDIR}/Util/UtilMysqlClient.o 
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilMysqlClient_nomain.o Util/UtilMysqlClient.cpp;\
+	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Util/UtilMysqlClient_nomain.o Util/UtilMysqlClient.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/Util/UtilMysqlClient.o ${OBJECTDIR}/Util/UtilMysqlClient_nomain.o;\
 	fi
@@ -285,7 +285,7 @@ ${OBJECTDIR}/main_nomain.o: ${OBJECTDIR}/main.o main.cpp
 	   (echo "$$NMOUTPUT" | ${GREP} 'T _main$$'); \
 	then  \
 	    ${RM} "$@.d";\
-	    $(COMPILE.cc) -O2 -I/usr/local/include/ -I../../boost_1_60_0 -I. -I. -I. -I. -I. -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_nomain.o main.cpp;\
+	    $(COMPILE.cc) -O2 -IUtil -I/usr/local/include/ -I/usr/include/ -I/usr/local/include/mysql++ -I/usr/include/mysql -I. -Dmain=__nomain -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_nomain.o main.cpp;\
 	else  \
 	    ${CP} ${OBJECTDIR}/main.o ${OBJECTDIR}/main_nomain.o;\
 	fi
